a8951148a728627bff1a0df4b94c6e9dd8a39103
Kevin Erbault <kerbault@z2r7p1.le-101.fr>
2018-02-21 23:52:10 +0100

    ceryneian

---
+++ 02_ceryneian_hind/ceryneian_hind.rb
@@ -0,0 +1,53 @@
+# **************************************************************************** #
+#                                                           LE - /             #
+#                                                               /              #
+#    ceryneian_hind.rb                                .::    .:/ .      .::    #
+#                                                  +:+:+   +:    +:  +:+:+     #
+#    By: kerbault <kerbault@student.le-101.fr>      +:+   +:    +:    +:+      #
+#                                                  #+#   #+    #+    #+#       #
+#    Created: 2018/02/21 15:37:51 by kerbault     #+#   ##    ##    #+#        #
+#    Updated: 2018/02/21 23:32:14 by kerbault    ###    #+. /#+    ###.fr      #
+#                                                          /                   #
+#                                                         /                    #
+# **************************************************************************** #
+
+require "oauth2"
+require "colorize"
+
+if ARGV.empty? || !File.exist?(ARGV[0]) || ARGV[1]
+	puts "USAGE:".red + " ruby ceryneian_hind.rb <SINGLE_VALID_FILE>"
+	exit
+end
+
+UID = ENV['API_UID']
+SECRET = ENV['API_SECRET']
+# Create the client with your credentials
+client = OAuth2::Client.new(UID, SECRET, site: "https://api.intra.42.fr")
+# Get an access token
+token = client.client_credentials.get_token
+
+file = ARGV[0]
+
+puts "Scanning...".green
+File.readlines(file).each do |line|
+	sleep (0.5)
+	login = line.strip
+	if login.empty?
+		next
+	end		
+	begin
+		response = token.get("/v2/users/#{login}")
+	rescue
+		puts "wrong login or api down".yellow
+		next
+	end
+	parse = response.parsed
+	location = parse["location"]
+	print parse["first_name"] + " " + parse["last_name"] + " (" + login
+	if location.to_s.empty?
+		puts ") isn't logged"
+	else
+		puts ") is logged on " + location
+	end
+end
+puts "Done".green
